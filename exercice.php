edd95b57e50f530d053906e0ada349572853ff33
DialloMaria <diallomariam0715@gmail.com>
2024-02-21 01:47:00 +0000

    exercice1 fini

---
+++ exercice.php
@@ -0,0 +1,15 @@
+<?php
+// Tableau d'entiers
+$tableau = array(2, 3, 4, 5);
+
+// Initialisation du produit à 1
+$produit = 1;
+
+// Parcourir le tableau pour calculer le produit
+foreach ($tableau as $tableau) {
+    $produit *= $tableau;
+}
+
+// Affichage du produit
+echo "Le produit de tous les éléments du tableau est : $produit";
+?>
